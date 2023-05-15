49602b32136950e204b8481ccaf1ccebeeab9462
aufcoder <129998732+aufcoder@users.noreply.github.com>
2023-05-15 06:18:34 +0300

    Delete folder/Models directory

--- folder/Models/DTO/AddNoteRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NotesApp.API.Models.DTO
-{
-    public class AddNoteRequest
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string ColorHex { get; set; }
-    }
-}
--- folder/Models/DTO/UpdateNoteRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NotesApp.API.Models.DTO
-{
-    public class UpdateNoteRequest
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string ColorHex { get; set; }
-    }
-}
